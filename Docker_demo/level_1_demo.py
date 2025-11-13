3feba914250cd3c7a1282414a751d42ef45727ae
rani <rani.com>
2025-11-13 11:06:33 +0000

    docker

---
+++ Docker_demo/level_1_demo.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+print("Hello world, I just used docker for the second time!")
+
+df = pd.DataFrame({
+    'name': ['Nirosh', 'Bouchra'],
+    'eyeCol': ["Brown", "Blue"]
+})
+
+print(df)
